639647de5386f368b0d496f4521bd9e14068c00b
Katarzyna Cependa <kasia.cependa@gmail.com>
2024-06-21 23:29:18 +0200

    register view

--- frontend/src/routing/Routings.tsx
+++ frontend/src/routing/Routings.tsx
@@ -3,11 +3,15 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 
 const Test = React.lazy(() => import("../pages/Test"));
+const Register = React.lazy(() => import("../pages/Register"));
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [{ path: "/test", element: <Test /> }],
+    children: [
+      { path: "/test", element: <Test /> },
+      { path: "/rejestracja", element: <Register /> },
+    ],
   },
 ]);
